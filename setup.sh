78da615692f66053434ee86bca01b312da1d21e3
Ravikant <ravikantpandey88@gmail.com>
2024-11-20 18:32:11 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,25 +4,24 @@
 set -e
 
 echo "Creating Python virtual environment..."
-python3 -m venv ./
 
 echo "Installing pipenv..."
-./bin/pip install pipenv
+pip install pipenv
 
 echo "Configuring pipenv with local Python..."
-./bin/pipenv --python ./bin/python
+pipenv --python python
 
 echo "Setting up pipenv environment..."
-./bin/pipenv install
+pipenv install
 
 echo "Installing client dependencies..."
 npm install --prefix client
 
 echo "Installing additional Python packages..."
-./bin/pipenv run pip install flask_bcrypt typing_extensions
+pipenv run pip install flask_bcrypt typing_extensions
 
 echo "Setting up Flask environment variables..."
-./bin/pipenv run bash -c 'export FLASK_APP=app.py && \
+pipenv run bash -c 'export FLASK_APP=app.py && \
 export FLASK_RUN_PORT=8080 && cd server && \
 { flask db init && echo "DB init successful"; } || echo "DB init failed, continuing..." && \
 { flask db migrate -m "initial migration" && echo "DB migrate successful"; } || echo "DB migrate failed, continuing..." && \
